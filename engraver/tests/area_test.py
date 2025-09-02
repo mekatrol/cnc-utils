8de418514d1f2110e71b1712ddd94d01c2fb2bb8
mekatrol <git@mekatrol.com>
2025-09-02 21:31:03 +1000

    removed fixtures

--- engraver/tests/area_test.py
+++ engraver/tests/area_test.py
@@ -5,36 +5,36 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..",
 from geometry.GeoUtil import GeoUtil, PointInt
 
 
-@pytest.fixture
-def point():
-    return lambda x, y: PointInt(x, y)
+def test_area_positive_triangle():
+    pts = [PointInt(0, 0), PointInt(4, 0), PointInt(4, 3)]
+    assert GeoUtil.area(pts) == 6
 
 
-@pytest.fixture
-def geo():
-    return GeoUtil
+def test_area_negative_triangle():
+    pts = [PointInt(0, 0), PointInt(4, 3), PointInt(4, 0)]
+    assert GeoUtil.area(pts) == -6
 
 
-def test_area_positive_triangle(geo, point):
-    pts = [point(0, 0), point(4, 0), point(4, 3)]
-    assert geo.area(pts) == 6
+def test_area_zero_line():
+    pts = [PointInt(0, 0), PointInt(1, 1), PointInt(2, 2)]
+    assert GeoUtil.area(pts) == 0
 
 
-def test_area_negative_triangle(geo, point):
-    pts = [point(0, 0), point(4, 3), point(4, 0)]
-    assert geo.area(pts) == -6
+def test_area_zero_line_duplicate_point():
+    pts = [PointInt(0, 0), PointInt(1, 1), PointInt(1, 1)]
+    assert GeoUtil.area(pts) == 0
 
 
-def test_area_zero_line(geo, point):
-    pts = [point(0, 0), point(1, 1), point(2, 2)]
-    assert geo.area(pts) == 0
+def test_area_zero_line_same_point():
+    pts = [PointInt(1, 1), PointInt(1, 1), PointInt(1, 1)]
+    assert GeoUtil.area(pts) == 0
 
 
-def test_area_square(geo, point):
-    pts = [point(0, 0), point(1, 0), point(1, 1), point(0, 1)]
-    assert geo.area(pts) == 1
+def test_area_square():
+    pts = [PointInt(0, 0), PointInt(1, 0), PointInt(1, 1), PointInt(0, 1)]
+    assert GeoUtil.area(pts) == 1
 
 
-def test_area_square_negative(geo, point):
-    pts = [point(0, 0), point(0, 1), point(1, 1), point(1, 0)]
-    assert geo.area(pts) == -1
+def test_area_square_negative():
+    pts = [PointInt(0, 0), PointInt(0, 1), PointInt(1, 1), PointInt(1, 0)]
+    assert GeoUtil.area(pts) == -1
